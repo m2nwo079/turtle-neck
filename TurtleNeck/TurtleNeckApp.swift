be2d981bea4bdf29eea398925f8daedb95409363
m2nwo079 <12345678m2nwo079@users.noreply.github.com>
2026-09-16 22:38:10 +0900

    Initial Commit

---
+++ TurtleNeck/TurtleNeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurtleNeckApp.swift
+//  TurtleNeck
+//
+//  Created by 이민우 on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct TurtleNeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
